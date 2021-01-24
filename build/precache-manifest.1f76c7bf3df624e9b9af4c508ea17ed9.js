f0e19fc130b58d46dbe9d3efdbd1f39b73fa1a42
Jane You <janeyou@Janes-MacBook-Pro.local>
2021-01-23 21:19:59 -0500

    changes

---
+++ build/precache-manifest.1f76c7bf3df624e9b9af4c508ea17ed9.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fce2b127bae0f555b9e9159e217fccf5",
+    "url": "/portfolio-project/index.html"
+  },
+  {
+    "revision": "38b63fe4fdeff427fa18",
+    "url": "/portfolio-project/static/css/3.ca87188b.chunk.css"
+  },
+  {
+    "revision": "25ef57cca8c11b1e953b",
+    "url": "/portfolio-project/static/css/main.f4adb798.chunk.css"
+  },
+  {
+    "revision": "b41c6d46db2e893acf3b",
+    "url": "/portfolio-project/static/js/2.67f0c9c4.chunk.js"
+  },
+  {
+    "revision": "38b63fe4fdeff427fa18",
+    "url": "/portfolio-project/static/js/3.0124e2c6.chunk.js"
+  },
+  {
+    "revision": "25ef57cca8c11b1e953b",
+    "url": "/portfolio-project/static/js/main.f5573c37.chunk.js"
+  },
+  {
+    "revision": "812c487a91c66d84156f",
+    "url": "/portfolio-project/static/js/runtime-main.f771bebf.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio-project/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "c57ed71a51f954ab82dbdec0aeaf01a1",
+    "url": "/portfolio-project/static/media/Communyco.c57ed71a.png"
+  },
+  {
+    "revision": "7c686dc5fb33717545c3c399fc967028",
+    "url": "/portfolio-project/static/media/Jane.7c686dc5.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio-project/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d35c8d140679602795f7f2c324baf670",
+    "url": "/portfolio-project/static/media/Pizza-Builder.d35c8d14.png"
+  },
+  {
+    "revision": "b624b450b89f44356ad52dfb2d6bdef9",
+    "url": "/portfolio-project/static/media/Recipe.b624b450.png"
+  },
+  {
+    "revision": "5aace10e1f630704953ceb431ca1f024",
+    "url": "/portfolio-project/static/media/Skills.5aace10e.jpeg"
+  },
+  {
+    "revision": "e200e517ba161010c1dd889b334633a8",
+    "url": "/portfolio-project/static/media/bmo-logo.e200e517.jpg"
+  },
+  {
+    "revision": "8e290eda4f0f220e99457ba6b896d577",
+    "url": "/portfolio-project/static/media/contact.8e290eda.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio-project/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "69f4b5d02eae292fafb47ecad21cdaf5",
+    "url": "/portfolio-project/static/media/sante-canada-.69f4b5d0.png"
+  },
+  {
+    "revision": "7ecae96481a46b5f290f27391c5c1704",
+    "url": "/portfolio-project/static/media/uOttawa.7ecae964.png"
+  }
+]);
