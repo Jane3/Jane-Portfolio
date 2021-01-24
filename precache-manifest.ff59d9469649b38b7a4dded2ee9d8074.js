b4a71059fcd9c3cd35852c17026379b2784c677f
Jane You <janeyou@Janes-MacBook-Pro.local>
2021-01-23 22:21:05 -0500

    Updates

---
+++ precache-manifest.ff59d9469649b38b7a4dded2ee9d8074.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fd94eaee1991a96a0204d2060ef7c29",
+    "url": "/Jane-Portfolio/index.html"
+  },
+  {
+    "revision": "38b63fe4fdeff427fa18",
+    "url": "/Jane-Portfolio/static/css/3.ca87188b.chunk.css"
+  },
+  {
+    "revision": "f4a34bbce0ea827f1b6f",
+    "url": "/Jane-Portfolio/static/css/main.3d92b4b9.chunk.css"
+  },
+  {
+    "revision": "b41c6d46db2e893acf3b",
+    "url": "/Jane-Portfolio/static/js/2.67f0c9c4.chunk.js"
+  },
+  {
+    "revision": "38b63fe4fdeff427fa18",
+    "url": "/Jane-Portfolio/static/js/3.0124e2c6.chunk.js"
+  },
+  {
+    "revision": "f4a34bbce0ea827f1b6f",
+    "url": "/Jane-Portfolio/static/js/main.25e9ca2f.chunk.js"
+  },
+  {
+    "revision": "a3ee8d3aa5e2a78eef96",
+    "url": "/Jane-Portfolio/static/js/runtime-main.708b6eab.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/Jane-Portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "c57ed71a51f954ab82dbdec0aeaf01a1",
+    "url": "/Jane-Portfolio/static/media/Communyco.c57ed71a.png"
+  },
+  {
+    "revision": "7c686dc5fb33717545c3c399fc967028",
+    "url": "/Jane-Portfolio/static/media/Jane.7c686dc5.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/Jane-Portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d35c8d140679602795f7f2c324baf670",
+    "url": "/Jane-Portfolio/static/media/Pizza-Builder.d35c8d14.png"
+  },
+  {
+    "revision": "b624b450b89f44356ad52dfb2d6bdef9",
+    "url": "/Jane-Portfolio/static/media/Recipe.b624b450.png"
+  },
+  {
+    "revision": "5aace10e1f630704953ceb431ca1f024",
+    "url": "/Jane-Portfolio/static/media/Skills.5aace10e.jpeg"
+  },
+  {
+    "revision": "e200e517ba161010c1dd889b334633a8",
+    "url": "/Jane-Portfolio/static/media/bmo-logo.e200e517.jpg"
+  },
+  {
+    "revision": "8e290eda4f0f220e99457ba6b896d577",
+    "url": "/Jane-Portfolio/static/media/contact.8e290eda.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Jane-Portfolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "69f4b5d02eae292fafb47ecad21cdaf5",
+    "url": "/Jane-Portfolio/static/media/sante-canada-.69f4b5d0.png"
+  },
+  {
+    "revision": "7ecae96481a46b5f290f27391c5c1704",
+    "url": "/Jane-Portfolio/static/media/uOttawa.7ecae964.png"
+  }
+]);
