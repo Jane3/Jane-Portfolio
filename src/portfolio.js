390f5946f55b2f739af4ef76da5ff620d64605c8
Jane You <janeyou@Janes-MacBook-Pro.local>
2021-02-10 10:26:09 -0500

    updates

--- src/portfolio.js
+++ src/portfolio.js
@@ -12,7 +12,7 @@ const greeting = {
   /* Your Summary And Greeting Section */
   username: "Jane You",
   title: "Hi there, I am Jane",
-  subTitle: emoji("I am a software developer with 2 years of experience debugging, maintaining and implementing new features to the existing applications and platforms written in React JS, Javascript, HTML, Node JS, CSS, Python and Java to improve user experience and make software updates. I am constantly looking for challenging problems to tackle on HackerRank and Leetcode, and working towards Udemy completion certificates in React JS, JavaScript, MongoDB and Node.js in my free time."),
+  subTitle: emoji("I am a software developer with 3 years of experience debugging, maintaining and implementing new features to the existing applications and platforms written in React JS, Javascript, HTML, Node JS, CSS, Python and Java to improve user experience and make software updates. I am constantly looking for challenging problems to tackle on HackerRank and Leetcode, and working towards Udemy completion certificates in React JS, JavaScript, MongoDB and Node.js in my free time."),
   resumeLink: "https://docs.google.com/document/d/1tjmBpeeSrLjXY9NGBOWpTPAiwH-31g1d8_pqzWFLTdE/edit?usp=sharing"
                
 };
